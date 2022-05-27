9f81cb0969811b3da42ccf987197cfe1f8044038
ghrwork <ghrwork@163.com>
2022-05-27 16:09:36 +0800

    feat: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,43 @@
 module.exports = {
-    globals: {
-        __PROJECTNAME__: true,
+    root: true,
+    env: {
+        node: true,
     },
-    extends: ['sifu/vue'],
-    rules: {
-        'no-var': 0,
-    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        //   "@vue/typescript/recommended",
+        //   "@vue/prettier",
+        //   "@vue/eslint-config-typescript"
+    ],
+    parser: 'vue-eslint-parser',
     parserOptions: {
-        parser: 'babel-eslint',
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        jsxPragma: 'React',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        'vue/no-v-html': 'off',
+        'vue/require-default-prop': 'off',
+        'vue/require-explicit-emits': 'off',
+        'vue/multi-word-component-names': 'off',
+        '@typescript-eslint/no-explicit-any': 'off', // any
+        'no-debugger': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off', // setup()
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'no-unused-vars': [
+            'error',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+            },
+        ],
     },
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg }>()
-      const count = ref(0)
+defineProps<{ msg: string }>()
+const count = ref(0)
 </script>
 
 <template>
